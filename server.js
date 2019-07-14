e98b0db666a6666de6c4076d70b823b5fd4418a0
Vincent Romine <vinjrom@live.com>
2019-07-14 14:51:21 -0500

    Add server paths

---
+++ server.js
@@ -0,0 +1,25 @@
+// Pull required dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+// Configure Express app
+var app = express();
+var PORT = process.env.PORT;
+
+// Public directory to access CSS files
+app.use(express.static(path.join(__dirname, './app/public')));
+
+// Add middleware for parsing incoming request bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+// Add application routes
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+// Listening Port
+app.listen(PORT, function() {
+    console.log('Friend Finder app is listening on PORT: ' + PORT);
+});
